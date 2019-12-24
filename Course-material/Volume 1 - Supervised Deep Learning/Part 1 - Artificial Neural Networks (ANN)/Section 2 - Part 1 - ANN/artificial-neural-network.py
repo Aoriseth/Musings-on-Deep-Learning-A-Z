c963d9f95c507e9e9198fa96ba4126096cf58bb0
LennartCockx <lennart.cockx@gmail.com>
2019-12-24 16:37:24 +0100

    Tested plotting and printing SciView

--- Course-material/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 2 - Part 1 - ANN/artificial-neural-network.py
+++ Course-material/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 2 - Part 1 - ANN/artificial-neural-network.py
@@ -2,6 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# %%
 dataset = pd.read_csv("Churn_Modelling.csv")
-X = dataset.iloc[:, [2, 3]].values
+X = dataset.iloc[:, 3:13].values
 Y = dataset.iloc[:, 4].values
+
+# %%
+print(X)
+print(Y)
+
+# %%
+plt.scatter([1,2,3] ,[5,5,8])
+plt.show()
