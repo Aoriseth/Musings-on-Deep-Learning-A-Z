986c546dad98b3aac2604e734482db2de164f5e7
LennartCockx <lennart.cockx@gmail.com>
2019-12-26 11:16:34 +0100

    Perform label encoding for string variables  - First string variables need to be transformed to numerical values. This is done using a LabelEncoder on the country and gender columns.  - Next we need to split the country variables because they are not correlated. This is done by creating dummy columns for each country with a ColumnTransformer.  - We need to remove the first column to prevent the dummy variable trap.

--- Course-material/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 2 - Part 1 - ANN/artificial-neural-network.py
+++ Course-material/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 2 - Part 1 - ANN/artificial-neural-network.py
@@ -5,12 +5,24 @@ import pandas as pd
 # %%
 dataset = pd.read_csv("Churn_Modelling.csv")
 X = dataset.iloc[:, 3:13].values
-Y = dataset.iloc[:, 4].values
+Y = dataset.iloc[:, 13].values
+
+# %%
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+label_encoder = LabelEncoder()
+X[:, 1] = label_encoder.fit_transform(X[:, 1])
+X[:, 2] = label_encoder.fit_transform(X[:, 2])
+transformer = ColumnTransformer(transformers=[("Country", OneHotEncoder(categories='auto'), [1])],
+                                remainder='passthrough')
+X = transformer.fit_transform(X)
+X = X[:, 1:]
 
 # %%
 print(X)
 print(Y)
 
 # %%
-plt.scatter([1, 2, 3], [5, 5, 8])
-plt.show()
+# plt.scatter([1, 2, 3], [5, 5, 8])
+# plt.show()
